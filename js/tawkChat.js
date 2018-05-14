1a33ef49ee935f74b0d7a5027636e4001417e8d7
David NIWEWE <davejuelz@gmail.com>
2018-05-14 22:25:35 +0200

    Added tawk chat app

--- js/tawkChat.js
+++ js/tawkChat.js
@@ -2,7 +2,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5aacf9c84b401e45400dd019/default';
+s1.src='https://embed.tawk.to/5af9ef6a5f7cdf4f0534378e/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
